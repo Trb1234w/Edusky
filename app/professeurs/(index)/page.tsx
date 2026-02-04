a64f69b9577129e9562844483e91a8a40bb395d4
Trb1234w <trbbarry51@gmail.com>
2026-02-04 01:09:13 +0000

    ajout de la ligne export

--- app/professeurs/(index)/page.tsx
+++ app/professeurs/(index)/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 
 import { ProfesseursHero } from "@/components/professeurs/ProfesseursHero"
 import { ProfesseursFilterWrapper } from "@/components/professeurs-filter-wrapper"
